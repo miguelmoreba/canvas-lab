326cc81ae3730293978df3aaa411e7cb5bac8f47
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-04-19 12:14:16 +0100

    SOme things done

--- canvas_start_point/public/index.js
+++ canvas_start_point/public/index.js
@@ -1,1 +1,30 @@
-TEST
+const app = function(){
+  const canvas = document.querySelector('#main-canvas');
+  const context = canvas.getContext('2d');
+
+
+  // const drawLine()
+
+  let mouseClicked = false;
+  const mouseReleased = true;
+
+  canvas.addEventListener('click', onMouseClick, false);
+  canvas.addEventListener('click', onMouseMove, false);
+
+  function onMouseClick(event){
+    mouseClicked = !mouseClicked;
+  }
+
+  function onMouseMove(event){
+    if (mouseClicked){
+      context.beginPath();
+      context.arc(event.x, event.y, 7.5, 0, Math.PI * 2, false);
+      context.stroke();
+    }
+  }
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', app);
